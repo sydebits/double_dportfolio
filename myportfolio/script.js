79907d7fb96d51a0a369e49be5dba4de9b1586de
sydebits <103610391+sydebits@users.noreply.github.com>
2023-04-01 00:44:37 +0100

    debugged

--- myportfolio/script.js
+++ myportfolio/script.js
@@ -1,3 +1,4 @@
+/**************Typing Skills*******************/
 var typed = new Typed(".multiple-text", {
   strings: [
     "Frontend Developer",
@@ -10,11 +11,19 @@ var typed = new Typed(".multiple-text", {
   backDelay: 1500,
   loop: true,
 });
-let menuIcon = document.querySelector('#menuIcon');
-let navlinks = document.querySelector('.navlinks')
 
+/**************Menu Icon Navbar*******************/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/**************Scroll Section Active Link*******************/
 let sections = document.querySelectorAll("section");
-let navbar = document.querySelectorAll("header nav a");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
   sections.forEach((sec) => {
@@ -24,7 +33,7 @@ window.onscroll = () => {
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navlinks.forEach((links) => {
+      navLinks.forEach((links) => {
         links.classList.remove("active");
         document
           .querySelector("header nav a[href*=" + id + "]")
@@ -32,8 +41,25 @@ window.onscroll = () => {
       });
     }
   });
+  /**************Sticky Header*******************/
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /**************Menu Icon toggle off when Clicked*******************/
+  // menuIcon.classList.toggle("bx-x");
+  // navbar.classList.toggle("active");
 };
 
-let header = document.querySelector("header");
+/**************Scroll Reveal*******************/
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+})
 
-header.classList.toggle("sticky", window.scrollY > 100);
+ScrollReveal().reveal('.hero-content, .container', {origin:'top'});
+ScrollReveal().reveal('.hero-image, .skills-container, .portfolio-box, .contact form', {origin:'bottom'});
+ScrollReveal().reveal('.hero-content h1, .about-img', {origin:'left'});
+ScrollReveal().reveal('.hero-content p, .about-content', {origin:'right'});
